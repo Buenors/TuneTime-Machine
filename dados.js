292dd3f106b512f09b082be79776774fea355293
Henrique Bueno <henriquebueno2005@gmail.com>
2024-09-08 19:40:16 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -1,130 +1,175 @@
 let dados = [
-    {
-        titulo : "Ariana Grande",
-        descricao : "Cantora, compositora e atriz norte-americana, conhecida por sua poderosa voz e estilo pop vibrante. Iniciou sua carreira na série infantil Victorious e rapidamente se tornou um ícone da música pop. Suas letras exploram temas como amor, desgosto, empoderamento feminino e autodescoberta.", 
-        albums : [
-            "Yours Truly - (2013)", 
-            "My Everything - (2014)", 
-            "Dangerous Woman - (2016)", 
-            "Sweetener - (2018)", 
-            "thank u, next - (2019)", 
-            "Positions - (2020)", 
-            "eternal sunshine (2024)"],
-        link   : "https://pt.wikipedia.org/wiki/Ariana_Grande",
-        spotify : "https://open.spotify.com/intl-pt/artist/66CXWjxzNUsdJxJ2JdwvnR?si=4a24eee97c64430c&nd=1&dlsi=b9c2fd588d144f80",
-        tags: "ariana grande yours truly my everything dangerous woman sweetener thank u next positions eternal sunshine pop R&B vocalist singer songwriter contemporary pop contemporary R&B pop R&B vocal powerhouse debut album second album third album fourth album fifth album sixth album upcoming album genre fusion",
+  {
+    titulo: "Ariana Grande",
+    descricao: "Cantora, compositora e atriz norte-americana, conhecida por sua poderosa voz e estilo pop vibrante. Iniciou sua carreira na série infantil Victorious e rapidamente se tornou um ícone da música pop. Suas letras exploram temas como amor, desgosto, empoderamento feminino e autodescoberta.",
+    albums: [
+      "Yours Truly - (2013)",
+      "My Everything - (2014)",
+      "Dangerous Woman - (2016)",
+      "Sweetener - (2018)",
+      "thank u, next - (2019)",
+      "Positions - (2020)",
+      "eternal sunshine (2024)"
+    ],
+    link: "https://pt.wikipedia.org/wiki/Ariana_Grande",
+    spotify: "https://open.spotify.com/intl-pt/artist/66CXWjxzNUsdJxJ2JdwvnR?si=4a24eee97c64430c&nd=1&dlsi=b9c2fd588d144f80",
+    tags: "ariana grande yours truly my everything dangerous woman sweetener thank u next positions eternal sunshine pop R&B vocalist singer songwriter contemporary pop contemporary R&B pop R&B vocal powerhouse debut album second album third album fourth album fifth album sixth album upcoming album genre fusion",
 
-    },
-    {
-        titulo: "The Weeknd",
-        descricao: "Cantor, compositor e produtor canadense, conhecido por sua voz sensual e melodias melancólicas. Suas letras exploram temas como amor, perda, vício e a vida noturna. Seu som, que combina elementos de R&B, pop e eletrônica, o tornou um dos artistas mais influentes da sua geração.",
-        albums: [
-            "House of Balloons", 
-            "Thursday", 
-            "Echoes of Silence", 
-            "Kiss Land", 
-            "Beauty Behind the Madness", 
-            "Starboy", 
-            "My Dear Melancholy,", 
-            "After Hours",   
-            "Dawn FM"],
-        link: "https://pt.wikipedia.org/wiki/The_Weeknd",
-        spotify : "https://open.spotify.com/intl-pt/artist/1Xyo4u8uXC1ZmMpatF05PJ",
-        tags: "the weeknd house of balloons thursday echoes of silence kiss land beauty behind the madness starboy my dear melancholy after hours dawn fm r&b pop singer songwriter contemporary r&b dark r&b alternative pop synth pop dark R&B genre fusion"
-    },
-    {
-        titulo: "Olivia Rodrigo",
-        descricao: "Cantora, compositora e atriz norte-americana, que se tornou um fenômeno pop com seu álbum de estreia 'Sour'. Suas letras exploram temas como coração partido, crescimento pessoal e a complexidade das relações adolescentes. Seu estilo musical combina elementos de pop, rock e alternativa.",
-        albums: [
-            "SOUR", 
-            "GUTS", 
-            "GUTS (spilled)"],
-        link: "https://pt.wikipedia.org/wiki/Olivia_Rodrigo",
-        spotify: "https://open.spotify.com/intl-pt/artist/1Xyo4u8uXC1ZmMpatF05PJ",
-        tags: "olivia rodrigo sour drivers license good 4 u deja vu traitor brutal pop pop punk alternative pop singer songwriter teen pop songwriter pop punk genre fusion guts spilled"
-    },
-    {
-        titulo: "Tate McRae",
-        descricao: "Cantora, compositora e dançarina canadense, conhecida por sua voz suave e melodias cativantes. Tate McRae ganhou destaque nas mídias sociais por seus vídeos de dança e covers de músicas, antes de assinar com a RCA Records. Sua música, que combina elementos de pop, R&B e eletrônico, explora temas como amor, desgosto e crescimento pessoal.",
-        albums: [
-          "i used to think i could fly - (2022)",
-          "THINK LATER - (2023)",
-        ],
-        link: "https://pt.wikipedia.org/wiki/Tate_McRae",
-        spotify: "https://open.spotify.com/intl-pt/artist/45dkTj5sMRSjrmBSBeiHym",
-        tags: "tate mcrae pop r&b singer songwriter canadian pop dance pop electropop dancer viral sensation tiktok i used to think i could fly THINK LATER",
-      },
-      {
-        titulo: "Madison Beer",
-        descricao: "Cantora e compositora norte-americana, conhecida por sua beleza e voz suave. Descoberta por Justin Bieber em um vídeo no YouTube, Madison rapidamente ganhou destaque na indústria musical. Seu estilo musical, que combina elementos de pop e R&B, é marcado por letras introspectivas e melodias cativantes.",
-        albums: [
-          "As She Pleases - (2018)",
-          "Life Support - (2021)",
-          "Silence Between Songs - (2023)"
-        ],
-        link: "https://pt.wikipedia.org/wiki/Madison_Beer",
-        spotify: "https://open.spotify.com/intl-pt/artist/2kRfqPViCqYdSGhYSM9R0Q",
-        tags: "madison beer pop r&b singer songwriter modelo instagram viral youtube As She Pleases Life Support Silence Between Songs"
-      },
-      {
-        titulo: "Demi Lovato",
-        descricao: "Cantora, compositora e atriz norte-americana, conhecida por sua voz poderosa e letras inspiradoras. Iniciou sua carreira na Disney Channel e desde então se tornou um ícone da música pop. Sua música aborda temas como amor, perda, saúde mental e empoderamento feminino.",
-        albums: [
-          "Don't Forget - (2008)",
-          "Here We Go Again - (2009)",
-          "Unbroken - (2011)",
-          "Demi - (2013)",
-          "Confident - (2015)",
-          "Tell Me You Love Me - (2017)",
-          "Dancing with the Devil... the Art of Starting Over - (2021)",
-          "HOLY FVCK - (2022)",
-          "REVAMPED - (2023)",
-        ],
-        link: "https://pt.wikipedia.org/wiki/Demi_Lovato",
-        spotify: "https://open.spotify.com/intl-pt/artist/6S2OmqARrzebs0tKUEyXyp",
-        tags: "demi lovato pop rock pop punk singer songwriter atriz disney channel disney disney channel original series saúde mental empoderamento feminino Don't Forget Here We Go Again Unbroken Demi Confident Tell Me You Love Me with the Devil... the Art of Starting Over HOLY FVCK REVAMPED"
-      },
-    {
-          titulo: "Sabrina Carpenter",
-          descricao: "Cantora, compositora e atriz norte-americana, conhecida por sua voz suave e letras introspectivas. Iniciou sua carreira na Disney Channel e desde então se tornou um ícone da música pop. Seu estilo musical, que combina elementos de pop, rock e eletrônico, é marcado por melodias cativantes e letras que exploram temas como amor, amizade e crescimento pessoal.",
-          albums: [
-            "Eyes Wide Open - (2015)",
-            "EVOLution - (2016)",
-            "Singular: Act I - (2018)",
-            "Singular: Act II - (2019)",
-            "emails i can't send - (2022)",
-            "Short n'Sweet - (2024)"
-          ],
-          link: "https://pt.wikipedia.org/wiki/Sabrina_Carpenter",
-          spotify:"https://open.spotify.com/intl-pt/artist/74KM79TiuVKeVCqs8QtB0B",
-          tags: "sabrina carpenter pop rock singer songwriter atriz disney channel disney pop electropop Eyes Wide Open  EVOLution Singular: Act I Singular: Act II emails i can't send Short n'Sweet"
-        },
-        {
-          titulo: "Post Malone",
-          descricao: "Cantor, compositor e rapper norte-americano, conhecido por sua voz rouca e estilo musical único que combina elementos de hip hop, pop e R&B. Suas letras exploram temas como amor, fama e a vida na estrada. Post Malone se tornou um dos artistas mais populares da sua geração.",
-          albums: [
-            "Stoney - (2016)",
-            "beerbongs&bentleys - (2018)",
-            "Hollywood's Bleeding - (2019)",
-            "Twelve Carat Toothache - (2022)",
-            "Austin - (2023)",
-            "F-1 Trillion - (2024)",
-          ],
-          link: "https://pt.wikipedia.org/wiki/wiki/Post_Malone",
-          spotify:"https://open.spotify.com/intl-pt/artist/246dkjvS1zLTtiykXe5h60",
-          tags: "post malone hip hop pop rap singer songwriter trap cloud rap mumble rapStoney beerbongs&bentleys Hollywood's Bleeding Twelve Carat Toothache Austin F-1 Trillion"
-        },
-        {
-          titulo: "Dua Lipa",
-          descricao: "Cantora e compositora britânica, conhecida por sua voz poderosa e estilo musical pop vibrante. Sua música, que combina elementos de disco, R&B e dance-pop, é marcada por melodias cativantes e letras que exploram temas como empoderamento feminino e amor. Dua Lipa se tornou um ícone da música pop mundial.",
-          albums: [
-            "Dua Lipa - (2017)",
-            "Future Nostalgia - (2020)",
-            "Radical Optimism - (2024)"   
-          ],
-          link: "https://pt.wikipedia.org/wiki/Dua_Lipa",
-          spotify:"https://open.spotify.com/intl-pt/artist/6M2wZ9GZgrQXHCFfjv46we",
-          tags: "dua lipa pop dance pop disco r&b singer songwriter brit pop future nostalgia radical optimism"
-        }
-
+  },
+  {
+    titulo: "The Weeknd",
+    descricao: "Cantor, compositor e produtor canadense, conhecido por sua voz sensual e melodias melancólicas. Suas letras exploram temas como amor, perda, vício e a vida noturna. Seu som, que combina elementos de R&B, pop e eletrônica, o tornou um dos artistas mais influentes da sua geração.",
+    albums: [
+      "House of Balloons - (2011)",
+      "Thursday - (2011)",
+      "Echoes of Silence - (2011)",
+      "Kiss Land - (2013)",
+      "Beauty Behind the Madness - (2015)",
+      "Starboy - (2016)",
+      "My Dear Melancholy, - (2018)",
+      "After Hours - (2020)",
+      "Dawn FM - (2022)",
+      "HURRY UP TOMORROW - (2024)"
+    ],
+    link: "https://pt.wikipedia.org/wiki/The_Weeknd",
+    spotify: "https://open.spotify.com/intl-pt/artist/1Xyo4u8uXC1ZmMpatF05PJ",
+    tags: "the weeknd house of balloons thursday echoes of silence kiss land beauty behind the madness starboy my dear melancholy after hours dawn fm r&b pop singer songwriter contemporary r&b dark r&b alternative pop synth pop dark R&B genre fusion"
+  },
+  {
+    titulo: "Olivia Rodrigo",
+    descricao: "Cantora, compositora e atriz norte-americana, que se tornou um fenômeno pop com seu álbum de estreia 'Sour'. Suas letras exploram temas como coração partido, crescimento pessoal e a complexidade das relações adolescentes. Seu estilo musical combina elementos de pop, rock e alternativa.",
+    albums: [
+      "SOUR - (2021)",
+      "GUTS - (2023)",
+      "GUTS (spilled) - (2024)"
+    ],
+    link: "https://pt.wikipedia.org/wiki/Olivia_Rodrigo",
+    spotify: "https://open.spotify.com/intl-pt/artist/1Xyo4u8uXC1ZmMpatF05PJ",
+    tags: "olivia rodrigo sour drivers license good 4 u deja vu traitor brutal pop pop punk alternative pop singer songwriter teen pop songwriter pop punk genre fusion guts spilled"
+  },
+  {
+    titulo: "Tate McRae",
+    descricao: "Cantora, compositora e dançarina canadense, conhecida por sua voz suave e melodias cativantes. Tate McRae ganhou destaque nas mídias sociais por seus vídeos de dança e covers de músicas, antes de assinar com a RCA Records. Sua música, que combina elementos de pop, R&B e eletrônico, explora temas como amor, desgosto e crescimento pessoal.",
+    albums: [
+      "i used to think i could fly - (2022)",
+      "THINK LATER - (2023)",
+    ],
+    link: "https://pt.wikipedia.org/wiki/Tate_McRae",
+    spotify: "https://open.spotify.com/intl-pt/artist/45dkTj5sMRSjrmBSBeiHym",
+    tags: "tate mcrae pop r&b singer songwriter canadian pop dance pop electropop dancer viral sensation tiktok i used to think i could fly THINK LATER",
+  },
+  {
+    titulo: "Madison Beer",
+    descricao: "Cantora e compositora norte-americana, conhecida por sua beleza e voz suave. Descoberta por Justin Bieber em um vídeo no YouTube, Madison rapidamente ganhou destaque na indústria musical. Seu estilo musical, que combina elementos de pop e R&B, é marcado por letras introspectivas e melodias cativantes.",
+    albums: [
+      "As She Pleases - (2018)",
+      "Life Support - (2021)",
+      "Silence Between Songs - (2023)"
+    ],
+    link: "https://pt.wikipedia.org/wiki/Madison_Beer",
+    spotify: "https://open.spotify.com/intl-pt/artist/2kRfqPViCqYdSGhYSM9R0Q",
+    tags: "madison beer pop r&b singer songwriter modelo instagram viral youtube As She Pleases Life Support Silence Between Songs"
+  },
+  {
+    titulo: "Demi Lovato",
+    descricao: "Cantora, compositora e atriz norte-americana, conhecida por sua voz poderosa e letras inspiradoras. Iniciou sua carreira na Disney Channel e desde então se tornou um ícone da música pop. Sua música aborda temas como amor, perda, saúde mental e empoderamento feminino.",
+    albums: [
+      "Don't Forget - (2008)",
+      "Here We Go Again - (2009)",
+      "Unbroken - (2011)",
+      "Demi - (2013)",
+      "Confident - (2015)",
+      "Tell Me You Love Me - (2017)",
+      "Dancing with the Devil... the Art of Starting Over - (2021)",
+      "HOLY FVCK - (2022)",
+      "REVAMPED - (2023)",
+    ],
+    link: "https://pt.wikipedia.org/wiki/Demi_Lovato",
+    spotify: "https://open.spotify.com/intl-pt/artist/6S2OmqARrzebs0tKUEyXyp",
+    tags: "demi lovato pop rock pop punk singer songwriter atriz disney channel disney disney channel original series saúde mental empoderamento feminino Don't Forget Here We Go Again Unbroken Demi Confident Tell Me You Love Me with the Devil... the Art of Starting Over HOLY FVCK REVAMPED"
+  },
+  {
+    titulo: "Sabrina Carpenter",
+    descricao: "Cantora, compositora e atriz norte-americana, conhecida por sua voz suave e letras introspectivas. Iniciou sua carreira na Disney Channel e desde então se tornou um ícone da música pop. Seu estilo musical, que combina elementos de pop, rock e eletrônico, é marcado por melodias cativantes e letras que exploram temas como amor, amizade e crescimento pessoal.",
+    albums: [
+      "Eyes Wide Open - (2015)",
+      "EVOLution - (2016)",
+      "Singular: Act I - (2018)",
+      "Singular: Act II - (2019)",
+      "emails i can't send - (2022)",
+      "Short n'Sweet - (2024)"
+    ],
+    link: "https://pt.wikipedia.org/wiki/Sabrina_Carpenter",
+    spotify: "https://open.spotify.com/intl-pt/artist/74KM79TiuVKeVCqs8QtB0B",
+    tags: "sabrina carpenter pop rock singer songwriter atriz disney channel disney pop electropop Eyes Wide Open  EVOLution Singular: Act I Singular: Act II emails i can't send Short n'Sweet"
+  },
+  {
+    titulo: "Post Malone",
+    descricao: "Cantor, compositor e rapper norte-americano, conhecido por sua voz rouca e estilo musical único que combina elementos de hip hop, pop e R&B. Suas letras exploram temas como amor, fama e a vida na estrada. Post Malone se tornou um dos artistas mais populares da sua geração.",
+    albums: [
+      "Stoney - (2016)",
+      "beerbongs&bentleys - (2018)",
+      "Hollywood's Bleeding - (2019)",
+      "Twelve Carat Toothache - (2022)",
+      "Austin - (2023)",
+      "F-1 Trillion - (2024)",
+    ],
+    link: "https://pt.wikipedia.org/wiki/wiki/Post_Malone",
+    spotify: "https://open.spotify.com/intl-pt/artist/246dkjvS1zLTtiykXe5h60",
+    tags: "post malone hip hop pop rap singer songwriter trap cloud rap mumble rapStoney beerbongs&bentleys Hollywood's Bleeding Twelve Carat Toothache Austin F-1 Trillion"
+  },
+  {
+    titulo: "Dua Lipa",
+    descricao: "Cantora e compositora britânica, conhecida por sua voz poderosa e estilo musical pop vibrante. Sua música, que combina elementos de disco, R&B e dance-pop, é marcada por melodias cativantes e letras que exploram temas como empoderamento feminino e amor. Dua Lipa se tornou um ícone da música pop mundial.",
+    albums: [
+      "Dua Lipa - (2017)",
+      "Future Nostalgia - (2020)",
+      "Radical Optimism - (2024)"
+    ],
+    link: "https://pt.wikipedia.org/wiki/Dua_Lipa",
+    spotify: "https://open.spotify.com/intl-pt/artist/6M2wZ9GZgrQXHCFfjv46we",
+    tags: "dua lipa pop dance pop disco r&b singer songwriter brit pop future nostalgia radical optimism"
+  },
+  {
+    titulo: "Billie Eilish",
+    descricao: "Cantora, compositora e produtora musical norte-americana, conhecida por sua voz grave e letras introspectivas. Alcançou fama mundial com o single 'Ocean Eyes' e se tornou uma das artistas mais influentes da sua geração.",
+    albums: [
+      "WHEN WE ALL FALL ASLEEP, WHERE DO WE GO? - (2019)",
+      "Happier Than Ever - (2021)",
+      "HIT ME HARD AND SOFT - (2024)",
+    ],
+    link: "https://pt.wikipedia.org/wiki/Billie_Eilish",
+    spotify: "spotify",
+    tags: "billie eilish when we all fall asleep where do we go happier than ever ocean eyes bad guy everything i wanted dont smile at me dont mess with me pop alternative R&B indie pop dark pop electropop"
+  },
+  {
+    titulo: "Camila Cabello",
+    descricao: "Cantora e compositora cubano-americana, conhecida por sua voz poderosa e estilo pop latino. Alcançou fama mundial como integrante do grupo Fifth Harmony e se tornou uma das artistas solo de maior sucesso da atualidade.",
+    albums: [
+      "Camila - (2018)",
+      "Romance - (2019)",
+      "Familia - (2022)",
+      "C,XOXO - (2024)",
+    ],
+    link: "https://pt.wikipedia.org/wiki/Camila_Cabello",
+    spotify: "spotify",
+    tags: "camila cabello camila romance familia fifth harmony havana señorita never be the same pop latin pop R&B"
+  },
+  {
+    titulo: "Lady Gaga",
+    descricao: "Cantora, compositora e atriz norte-americana, conhecida por sua excentricidade, performances extravagantes e voz poderosa. Alcançou fama mundial com hits como 'Poker Face' e 'Bad Romance', revolucionando a indústria da música com seu estilo único e inovador.",
+    albums: [
+      "The Fame - (2008)",
+      "The Fame Monster - (2009)",
+      "Born This Way - (2011)",
+      "ARTPOP - (2013)",
+      "Joanne - (2016)",
+      "A Star Is Born - (2018)",
+      "Chromatica - (2020)",
+    ],
+    link: "https://pt.wikipedia.org/wiki/Lady_Gaga",
+    spotify: "spotify",
+    tags: "lady gaga the fame the fame monster born this way artopop joanne a star is born chromatica poker face bad romance just dance telephone pop dance-pop electronic music experimental music fashion icon"
+  },
 ];
+
